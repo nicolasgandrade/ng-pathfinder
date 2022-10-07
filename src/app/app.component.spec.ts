d73a43cc3527c2509a8af25b3066029e7e417ef9
nicolasgandrade <nicolasguerrero72@gmail.com>
2022-10-07 16:32:39 -0300

    Create basic test to verify if jest setup worked

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,21 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = new AppComponent();
+  });
+
+  it('should have Ng Pathfinder as title', () => {
+    expect(fixture.title).toEqual("Ng Pathfinder");
   });
 });
